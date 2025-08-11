af679710f3f7543f824e8415f2ea8604c5dc3c27
Aurele Habbard <ahabbard@f1r4s10.clusters.42paris.fr>
2025-08-12 01:31:14 +0200

    Correction du cas n = 1

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: ahabbard <ahabbard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 16:49:24 by ahabbard          #+#    #+#             */
-/*   Updated: 2025/08/11 22:33:52 by ahabbard         ###   ########.fr       */
+/*   Updated: 2025/08/12 01:29:21 by ahabbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void	ft_reset(int *position, int *size, char *comb)
 	int	index;
 
 	index = *position;
-	comb[index] = comb[index] + 1;
+	if (!ft_max_value(position, size, comb))
+		comb[index] = comb[index] + 1;
 	while (index < *size - 1)
 	{
 		comb[index + 1] = comb[index] + 1;
@@ -32,19 +33,14 @@ void	ft_reset(int *position, int *size, char *comb)
 
 void	ft_init(int *index, int *size, char *comb)
 {
-	if (*size != 1)
+	while (*index != *size)
 	{
-		while (*index != *size)
-		{
-			comb[*index] = *index + 48;
-			(*index)++;
-		}
-		(*index)--;
-		comb[*size] = ',';
-		comb[*size + 1] = ' ';
+		comb[*index] = *index + 48;
+		(*index)++;
 	}
-	else
-		comb[*index] = 48;
+	(*index)--;
+	comb[*size] = ',';
+	comb[*size + 1] = ' ';
 }
 
 void	ft_print_combn(int size)
@@ -55,15 +51,16 @@ void	ft_print_combn(int size)
 	index = 0;
 	ft_init(&index, &size, comb);
 	write(1, comb, size + 2);
-	while (comb[0] != 48 + 9 - size + 1 && size != 1)
+	while (comb[0] != 48 + 9 - size + 1)
 	{
 		index = size - 1;
-		while (ft_max_value(&index, &size, comb))
+		while (!ft_max_value(&index, &size, comb))
 		{
 			ft_reset(&index, &size, comb);
-			write(1, comb, size + 2);
+			if (!ft_max_value(&index, &size, comb))
+				write(1, comb, size + 2);
 		}
-		while (index > 0 && !ft_max_value(&index, &size, comb))
+		while (index > 0 && ft_max_value(&index, &size, comb))
 			index--;
 		ft_reset(&index, &size, comb);
 		if (comb[0] == 48 + 9 - size + 1)
@@ -74,19 +71,19 @@ void	ft_print_combn(int size)
 }
 
 /*
-int	main(void)
+nt	main(void)
 {
-	ft_print_comb(1);
+	ft_print_combn(1);
 	write(1, "\n_________________\n", 20);
-	ft_print_comb(4);
+	ft_print_combn(3);
 	write(1, "\n__________________\n", 20);
-	ft_print_comb(9);
+	ft_print_combn(9);
 	write(1, "\n__________________\n", 20);
-	ft_print_comb(10);
+	ft_print_combn(10);
 	write(1, "\n__________________\n", 20);
-	ft_print_comb(0);
+	ft_print_combn(0);
 	write(1, "___________________\n", 20);
-	ft_print_comb(-1);
+	ft_print_combn(-1);
 	return (0);
 }
-*/
+ */
